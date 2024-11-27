70facb7c8f4a76b15d15c36d49833a0ae95e28bd
Lemonnik6484 <LemonGamer6484@gmail.com>
2024-11-27 08:34:38 +0100

    oops

--- neoforge-neogradle/1.21.1/files/java/ExampleMod.java
+++ neoforge-neogradle/1.21.1/files/java/ExampleMod.java
@@ -45,13 +45,13 @@ public class %mkmod:main%
     
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public ExampleMod(IEventBus modEventBus, ModContainer modContainer)
+    public %mkmod:main%(IEventBus modEventBus, ModContainer modContainer)
     {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
         // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
+        // Note that this is necessary if and only if we want *this* class (%mkmod:main%) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
